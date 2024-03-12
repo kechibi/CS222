2f63c3168932471c3c45d111a602356c9567240b
Andre <132373584+kechibi@users.noreply.github.com>
2024-03-12 11:18:09 +0800

    Add files via upload

---
+++ mp4/mp4.asm
@@ -0,0 +1,87 @@
+section .data
+    newline db 10
+
+section .text
+    global _start
+
+    print_char:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, esp
+        mov edx, 1
+        int 0x80
+        ret
+
+    _start:
+        cmp al, 'A'
+        jl not_uppercase
+        cmp al, 'Z'
+        jg not_uppercase
+        add al, 32
+
+    not_uppercase:
+        push eax
+        call print_char
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, newline
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg1
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, newline
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg2
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, newline
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg3
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, newline
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg4
+        mov edx, 1
+        int 0x80
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, newline
+        mov edx, 1
+        int 0x80
+
+        mov eax, 1
+        xor ebx, ebx
+        int 0x80
+
+    section .data
+        msg1 db 'A'
+        msg2 db 'a'
+        msg3 db 'Z'
+        msg4 db 'z'
